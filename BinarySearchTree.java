23baad640c1f01b1665c0b49da7800a97ea8dab6
Khushi Gupta <60938701+kushi06@users.noreply.github.com>
2022-09-07 16:44:23 +0530

    Add files via upload

---
+++ BinarySearchTree.java
@@ -0,0 +1,52 @@
+public class BinarySearchTree {
+
+    static class Node{
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data){
+            this.data = data;
+        }
+    }
+    public static Node insert(Node root, int val){
+        if(root == null){
+            root = new Node(val);
+            return root;
+        }
+
+        if(root.data > val){
+            //leftsubtree
+            root.left = insert(root.left, val);
+        }
+        else{
+            //rightsubtree
+            root.right = insert(root.right, val);
+        }
+        return root;
+    }
+
+    public static void Inorder(Node root){
+        if(root == null){
+            return;
+        }
+        Inorder(root.left);
+        System.out.print(root.data +" ");
+        Inorder(root.right);
+
+    }
+    public static void main(String[] args) {
+        int[] values = {5, 1, 3, 4, 2, 7};
+        Node root = null;
+        
+        for (int i=0; i < values.length ; i++) {
+            root = insert(root, values[i]);
+        }
+        
+        Inorder(root);
+        System.out.println();
+     
+
+    }
+    
+}
